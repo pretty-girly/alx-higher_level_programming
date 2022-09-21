1e06daf31f7cbe3d6e0ff9bf6884d9e939b92361
Agatha Enyadike <agathaenyadike01@gmail.com>
2022-09-21 02:17:04 -0700

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # 1-rectangle.py
-"""Defines a Rectangle class."""
+"""Defines a Reictangle class."""
 class Rectangle:
+
     """Represent a rectangle."""
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
